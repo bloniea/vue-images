d50fdb5ac4278c5451450b1648fa27c2879d58de
bloniea <bloniea@qq.com>
2024-06-09 17:32:31 +0800

    修改部分ui

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -6,14 +6,14 @@ export const config = {
 
   url: <string>'https://images.bloniea.com/', // 图片地址前缀
   title: 'bloniea 姉さま的图床',
-  loading_url: 'https://images.bloniea.com/images1/loading.gif', // 页面的load gif图片
-  default_avatar: 'https://images.bloniea.com/images1/default_avatar.png',
+  loading_url: 'https://images.bloniea.com//images/web/loading_1717925494545_781.gif', // 页面的load gif图片
+  default_avatar: 'https://images.bloniea.com/images/web/_1717925180824_349.png',
   maxsize: 20, // 支持的图片大小,单位mb
   maxLen: 8, //单次最多选择的图片数量
   path: 'images', // 存放仓库的原图路径
   thumbnailPath: 'imagesThumbnail', // 存放仓库的缩略图路径
   message: 'image upload', // github 的message信息
-  avatar: 'https://images.bloniea.com/images1/avatar.png',
+  avatar: 'https://images.bloniea.com//images/web/_1717925124500_918.png',
   defaultCategoryId: 1,
   copyright: `©2023-${new Date().getFullYear()} bloniea. All rights reserved.`,
   subsectionSize: 4, //分段上传,每段大小,单位mb
